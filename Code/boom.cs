49e8fab46c79ba95d742dda391e105e33c23622c
renhao <hao_ren@histudiogames.com>
2019-03-08 11:24:37 +0800

    初始化

---
+++ Code/boom.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Croe.Work
+{
+    public class CoreSet
+    {
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        public void Init()
+        {
+
+        }
+
+        /// <summary>
+        /// 后退一步
+        /// </summary>
+        public void BackStep()
+        {
+
+        }
+
+
+        public void Boom()
+        {
+
+        }
+
+
+        public string GetSaveData()
+        {
+            string info = string.Empty;
+
+
+
+            return info;
+        }
+
+        public bool SetSaveData()
+        {
+
+
+            return false;
+        }
+    }
+}

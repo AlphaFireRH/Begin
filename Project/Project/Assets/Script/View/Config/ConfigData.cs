9e143c6173fc6e7500e8f0a28725cbf9a6d4d77b
virgilwei <virgilweimail@gmail.com>
2019-03-12 11:57:13 +0800

    地图初始化

---
+++ Project/Project/Assets/Script/View/Config/ConfigData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ConfigData
+{
+    /// <summary>
+    /// 地图大小
+    /// </summary>
+    public const float MAP_SIZE = 130f;
+
+}

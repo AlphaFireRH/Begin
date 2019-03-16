e5e3f52939c49a41ce60fa30bb6e3383dddfe016
hao_ren <hao_ren@histudiogames.com>
2019-03-16 23:10:15 +0800

    声音控制器

--- Project/Project/Assets/Script/Controller/AudioController.cs
+++ Project/Project/Assets/Script/Controller/AudioController.cs
@@ -4,6 +4,50 @@ using UnityEngine;
 
 public class AudioController : SingleMono<AudioController>
 {
+    public bool soundState = true;
 
 
+    public void PlaySound(AudioType type)
+    {
+        if (!soundState)
+        {
+            return;
+        }
+        switch (type)
+        {
+            case AudioType.click:
+                {
+
+                }
+                break;
+            case AudioType.merge:
+                {
+
+                }
+                break;
+            case AudioType.move:
+                {
+
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+public enum AudioType
+{
+    /// <summary>
+    /// 点击
+    /// </summary>
+    click,
+    /// <summary>
+    /// 有合并
+    /// </summary>
+    merge,
+    /// <summary>
+    /// 移动（未合并）
+    /// </summary>
+    move
 }
